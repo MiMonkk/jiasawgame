ed4d0d4185c7c2a317396c7e71bb83164a9369bc
QQF <2776938680@qq.com>
2024-07-31 21:24:22 +0800

    初步了图片

--- src/com/qifan/ui/GameJFrame.java
+++ src/com/qifan/ui/GameJFrame.java
@@ -1,16 +1,64 @@
 package com.qifan.ui;
 
 import javax.swing.*;
+import java.util.Random;
 
 public class GameJFrame extends JFrame {
+    int[][] data = new int[4][4];
+
     public GameJFrame(){
         // 初始化界面
         initJFrame();
+
         // 初始化菜单
         initJMenuBar();
 
+        // 初始化数据（打乱）
+        initData();
+        // 初始化图片
+        initImage();
+
 
         this.setVisible(true);
+
+    }
+
+    private void initData() {
+        int[] tempArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        Random r = new Random();
+        for (int i = 0; i < tempArr.length; i++) {
+            int index = r.nextInt(tempArr.length);
+            int temp = tempArr[i];
+            tempArr[i] = tempArr[index];
+            tempArr[index] = temp;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                data[i][j] = tempArr[i * 4 + j];
+            }
+        }
+    }
+
+    private void initImage() {
+
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                // 创建一个图片Image的对象
+                ImageIcon icon = new ImageIcon("C:\\Users\\chinf\\IdeaProjects\\jigsawgame\\image\\animal\\animal3\\" + data[i][j] +".jpg");
+                // 创建一个JLabel的对象（管理容器）
+                JLabel label = new JLabel(icon);
+                // 指定图片位置
+                label.setBounds(105 * j, 105 * i, 105, 105);
+
+                this.getContentPane().add(label);
+            }
+        }
+
+
+
+        // 把管理容器添加到界面中
+        //this.add(label);
 
     }
 
@@ -44,6 +92,7 @@ public class GameJFrame extends JFrame {
         this.setAlwaysOnTop(true);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
     }
 
 }
