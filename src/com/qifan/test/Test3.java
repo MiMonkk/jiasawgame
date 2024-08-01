c4b78d33df8ae27807394377b63713c546fe9df3
QQF <2776938680@qq.com>
2024-08-01 21:01:30 +0800

    初步了图片

---
+++ src/com/qifan/test/Test3.java
@@ -0,0 +1,41 @@
+package com.qifan.test;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Test3 {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        //设置界面的宽高
+        jFrame.setSize(603, 680);
+        //设置界面的标题
+        jFrame.setTitle("事件演示");
+        //设置界面置顶
+        jFrame.setAlwaysOnTop(true);
+        //设置界面居中
+        jFrame.setLocationRelativeTo(null);
+        //设置关闭模式
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //取消默认的居中放置，只有取消了才会按照XY轴的形式添加组件
+        jFrame.setLayout(null);
+
+        // 创建按钮
+        JButton jtb = new JButton();
+        // 设置位置
+        jtb.setBounds(0,0,100,50);
+        // 添加动作监听
+        jtb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("click!");
+            }
+        });
+        // 按钮添加到界面中
+        jFrame.getContentPane().add(jtb);
+
+
+        jFrame.setVisible(true);
+
+    }
+}
